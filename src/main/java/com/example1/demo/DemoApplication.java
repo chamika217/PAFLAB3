1299f0d38ef9e6a442f5edc63669969d07da9058
chamika217 <disanayakedilshan7@gmail.com>
2026-02-19 16:08:41 +0530

    first commit

---
+++ src/main/java/com/example1/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String rootEndpoint() {
+		String message="Hello World!";
+		return message;
+	}
+	
+
+}
